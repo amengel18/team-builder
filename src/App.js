029f46abf1e3328ef41cfd2f68752cc207797e6b
Angela Engel <amengel.ae@icloud.com>
2021-02-14 12:11:24 -0700

    MVP I think

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from './Form';
+import TeamMember from './TeamMember'
 
-const initialMemberList = {
+const initialForm = {
   name: "",
   email: "",
   role: ""
 }
 
+
 function App() {
   
-  const [member, setMember] = useState([])
-  const [memberList, setMemberList] = useState(initialMemberList)
+  const [teamMembers, setTeamMembers] = useState([])
+  const [form, setForm] = useState(initialForm)
+  
+  const submit = () => {
+    setTeamMembers([...teamMembers, form])
+    setForm(initialForm)
+  }
 
-  return (
-      <div className="form-container">
-        <Form 
-        values={initialMemberList}/>
-      </div>
-  )
+  const update = (name, value) => {
+    setForm({...form, [name]: value})
+  }
+
+
+return (
+  <div className="form-container">
+    <h1>Team Member Form</h1>
+    <Form form={form} update={update} submit={submit} />
+    {
+      teamMembers.map(teamMember => {
+        return (
+          <TeamMember key={teamMember.id} details={teamMember} />
+        )
+      })
+    }
+  </div>
+)
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function Form(props){ 
+
+const {form, update, submit} = props
+
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    submit()
+  }
+
+  const handleChange = event => {
+    const {name, value} = event.target;
+    update(name, value);
+  }
+
+  return (
+    <div className='container'>
+     
+      <form className="form container" onSubmit={handleSubmit}>
+        <div className="form-group inputs">
+          <label>
+            Username
+            <input 
+              type="text"
+              name="username"
+              placeholder="type a username..."
+              value={form.username}
+              onChange={handleChange}/>
+          </label>
+          <label>
+            Email
+            <input 
+              type="email"
+              name="email"
+              placeholder="type an email..."
+              value={form.email}
+              onChange={handleChange}/>
+          </label>
+          <label>
+            Role
+            <select
+              name="role"
+              value={form.role}
+              onChange={handleChange}>
+              <option value="">-- Select a Role --</option>
+              <option value="Student">Student</option>
+              <option value="Instructor">Instructor</option>
+              <option value="Alumni">Alumni</option>
+            </select>
+          </label>
+      </div>
+      <div className="submit">
+        <button>Submit</button>
+      </div>
+    </form>
+  </div>
+  )
+}
